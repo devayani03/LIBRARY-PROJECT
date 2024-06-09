970389e4aef166a2ab8fd58433f5b99baeab52ea
devayani03 <drchandane_b21@ce.vjti.ac.in>
2024-06-09 16:17:17 +0530

    new commit

---
+++ library-mini-project/library1/src/main/java/com/example/library1/Library1Application.java
@@ -0,0 +1,12 @@
+package com.example.library1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Library1Application.class, args);
+	}
+}
